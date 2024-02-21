55f7da9ef880901a5ffd0e2d9b4cf59b244b7d55
curtiszahniser <144561801+curtiszahniser@users.noreply.github.com>
2024-02-20 18:32:44 -0600

    modified:   package-lock.json         modified:   package.json         modified:   src/App.jsx         modified:   src/main.jsx

---
+++ React-Portfolio/src/components/Project.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Project = (props) => {
+  return (
+    <div>
+        <div>props.title</div>
+        <div>props.</div>
+
+    </div>
+  )
+}
+
+export default Project
---
+++ React-Portfolio/src/pages/Portfolio.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const myProjects = []
+
+const Portfolio = () => {
+  return (
+    <div>
+        <div>{myProjects.map((proj)=>{
+            <Project= Project/>
+        })}</div>
+    </div>
+  )
+}
+
+export default Portfolio
